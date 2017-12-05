d0252086aa411fbd2fd66c34822c070815f60933
Fco. Javier Guerrero Carmona <fjguerreroc@icloud.com>
2017-12-05 20:28:03 +0100

    Initial Commit

---
+++ Lab1Integration/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Lab1Integration
+//
+//  Created by Fco. Javier Guerrero Carmona on 5/12/17.
+//  Copyright © 2017 Fco. Javier Guerrero Carmona. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
